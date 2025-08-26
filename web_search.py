0c303550959aea12e6c2e264c9e7ce3c0a77bdc1
James Chen <chenhao540@gmail.com>
2025-08-25 22:15:37 -0700

    create tools

--- web_search.py
+++ web_search.py
@@ -2,11 +2,24 @@ import os
 
 import httpx
 from mcp.server import FastMCP
+from google import genai
 
-api_key = os.getenv("API_KEY")
+from dotenv import load_dotenv
+
+load_dotenv()
+
+api_key = os.getenv("API_KEY", "")
+if api_key == "":
+    exit(1)
 
 # # 初始化 FastMCP 服务器
 app = FastMCP('web-search')
+
+api_key_gemini = os.getenv("API_KEY_GEMINI", "")
+if api_key_gemini == "":
+    exit(1)
+
+client = genai.Client(api_key=api_key_gemini)
 
 @app.tool()
 async def web_search(query: str) -> str:
@@ -43,6 +56,25 @@ async def web_search(query: str) -> str:
                     res_data.append(result['content'])
 
         return '\n\n\n'.join(res_data)
-    
+
+
+@app.tool()
+async def web_search_gemini(query: str) -> str:
+    """
+    搜索互联网内容
+
+    Args:
+        query: 要搜索内容
+
+    Returns:
+        搜索结果的总结
+    """
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=query
+    )
+
+    return response.text if response.text else "nothing"
+
 if __name__ == "__main__":
     app.run(transport='stdio')
